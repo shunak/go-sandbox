946b4797a114c0699762e38e2b6ec42e4ecf8b07
shunak <shuna966@gmail.com>
2020-02-28 15:45:10 +0900

    200228

---
+++ mkdir/main.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	"os"
+	"syscall"
+)
+
+func main(){
+	defaultUmask:=syscall.Umask(0)
+	os.Mkdir("/tmp/go-dir2",0777)
+	syscall.Umask(defaultUmask)
+	//print(defaultUmask)
+}
